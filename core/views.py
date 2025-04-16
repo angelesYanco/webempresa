6e6e4e867d18c0c231f326ec5c18ed6126ff9483
Toño Peña <angeles_yanco@hotmail.com>
2025-04-16 15:17:49 -0600

    Start project webempres, first commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+"""
+Inicio home/
+Historia about/
+Servicios services/
+Visítanos store/
+Contacto contact/
+Blog blog/
+Sample sample/ (esta es para páginas de prueba)
+"""
