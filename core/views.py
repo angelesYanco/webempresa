02cacaeabb4ef0d17cd41fee6d89223b606424a7
Toño Peña <angeles_yanco@hotmail.com>
2025-04-16 15:29:17 -0600

    Se crearon las vistas de la app 'core'

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
 """
 Inicio home/
 Historia about/
@@ -11,3 +10,24 @@ Contacto contact/
 Blog blog/
 Sample sample/ (esta es para páginas de prueba)
 """
+
+def home(request):
+    return HttpResponse("Inicio")
+
+def about(request):
+    return HttpResponse("Historia")
+
+def services(request):
+    return HttpResponse("Servicios")
+
+def store(request):
+    return HttpResponse("Visítanos")
+
+def contact(request):
+    return HttpResponse("Contacto")
+
+def blog(request):
+    return HttpResponse("Blog")
+
+def sample(request):
+    return HttpResponse("Sample")
