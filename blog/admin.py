7289915b68fd1155ba864ac6dc18db0b85d94458
Toño Peña <angeles_yanco@hotmail.com>
2025-05-09 14:14:18 -0600

    42. App Blog con relaciones Categoria->Post.

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post, Category
+
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+class PostAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Post, PostAdmin)
